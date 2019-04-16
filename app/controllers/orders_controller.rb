c29bc160e72383ba38c1bd88f39c529d243eb802
LisaElf <lisa11@posteo.de>
2019-04-16 21:28:26 +0200

    4.7 Revision

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
   def index
     @orders = Order.includes(:product).all
   end
@@ -8,12 +9,42 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order = Order.new
   end
 
+# POST /orders
+# POST /orders.json
   def create
+    @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to @order, notice: 'Order was successfully created.'}
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to @order, notice: 'Order was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def order_params
+    params.require(:order).permit(:user_id, :product_id, :total)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
 
-  resources :orders, only: [:index, :show, :create, :destroy]
+  resources :orders, only: [:index, :show, :create, :new, :destroy]
 end
