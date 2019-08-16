d16f5a6eb3fde8c108a1ef3814791a8303b69364
LisaElf <lisa11@posteo.de>
2019-08-16 15:42:25 +0200

    5.2 form search bar

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   has_many :orders
+
+  def self.search(search_term)
+    if :development
+      Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      Product.where("name ilike ?", "%#{search_term}%")
+    end
+  end
 end
