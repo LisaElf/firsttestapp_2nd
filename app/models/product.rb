087b120e630515dc8dd223b0d8ccc3c0d3dc9b3c
LisaElf <lisa11@posteo.de>
2019-08-28 21:16:28 +0200

    5.2  search form adapt ilike

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :orders
 
   def self.search(search_term)
-    if :development
+    if Rails.env.development?
       Product.where("name LIKE ?", "%#{search_term}%")
     else
       Product.where("name ilike ?", "%#{search_term}%")
