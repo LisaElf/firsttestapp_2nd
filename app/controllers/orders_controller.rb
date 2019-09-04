7a8fc7b12d7802a0e2e2aeb06d9e1835dfc70290
LisaElf <lisa11@posteo.de>
2019-09-04 22:08:14 +0200

    5.4 Authentication Routes Changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :set_order, only: [:show, :edit, :update, :destroy]
   def index
     @orders = Order.includes(:product).all
   end
